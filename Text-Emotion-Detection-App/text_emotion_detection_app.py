a1ddd587901db77ac63c84116d9dae4a55240e26
Sathya Lahari nuka <sathyalahari.nuka@gmail.com>
2023-12-02 10:53:26 -0600

    deleting unnecessary files

--- Text-Emotion-Detection-App/text_emotion_detection_app.py
+++ Text-Emotion-Detection-App/text_emotion_detection_app.py
@@ -1,52 +1,15 @@
 import streamlit as st
-
 import pandas as pd
 import numpy as np
 import altair as alt
 import pickle
-import joblib
-import requests
 import pathlib
-import os
-
-from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
-
-#enter credentials
-account_name = 'emotiondetectionblob'
-account_key = '3MeAhHc/2K8HSxUa8YsElqgGLfYVq6QDmeO3PtvSMA1GQhAU3Mav6RcnSyPnxgRnf/8bVN2tdF2Y+AStsgul2Q=='
-container_name = 'textemotionpickle'
-
-# create a client to interact with blob storage
-connect_str = 'DefaultEndpointsProtocol=https;AccountName=' + account_name + ';AccountKey=' + account_key + ';EndpointSuffix=core.windows.net'
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
 f = open(pathlib.Path(__file__).parent/'emotion_model.pkl', 'rb')
 pipe_lr = pickle.load(f)
 
-# def download_pickle_from_url(blob_url, save_path):
-#     response = requests.get(blob_url)
-#     if response.status_code == 200:
-#         with open(save_path, 'wb') as file:
-#             file.write(response.content)
-#     else:
-#         # Handle the error, raise an exception, or return None as needed
-#         raise Exception(f"Failed to download pickle file. Status code: {response.status_code}")
-#
-# # Example usage:
-# blob_url = 'https://emotiondetectionblob.blob.core.windows.net/textemotionpickle/text_emotion.pkl?sp=r&st=2023-12-02T05:06:54Z&se=2023-12-02T13:06:54Z&spr=https&sv=2022-11-02&sr=b&sig=NkDFqJwM%2Ba%2BCqtx0%2BfFZ0TxOiNic4av7pDuRI%2BVoB%2FQ%3D'
-# # save_path = 'C:/Users/sathy/OneDrive/Desktop/Fall-2023/Cloud Computing/TextEmotionDetection/Text-Emotion-Detection-App/text_emotion.pkl'
-# save_path = 'https://github.com/Sathyalahari/TextEmotionDetection/blob/main/Text-Emotion-Detection-App/text_emotion.pkl'
-# try:
-#     download_pickle_from_url(blob_url, save_path)
-#     pipe_lr = joblib.load(open(save_path, 'rb'))
-#     # Now you can use the loaded model in your application
-# except Exception as e:
-#     print(f"Error: {str(e)}")
-
 emotions_emoji_dict = {"anger": "😠", "disgust": "🤮", "fear": "😨😱", "happy": "🤗", "joy": "😂", "neutral": "😐", "sad": "😔",
                        "sadness": "😔", "shame": "😳", "surprise": "😮"}
-
 
 def predict_emotions(docx):
     results = pipe_lr.predict([docx])
@@ -91,10 +54,6 @@ def main():
 
             fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
             st.altair_chart(fig, use_container_width=True)
-
-
-
-
 
 
 if __name__ == '__main__':
